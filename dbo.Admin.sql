627a73edcc5f5a01d80ebffd6df5fc4587a2b8f9
stefani grigoriadou <stefanie_gregoriadou@hotmail.com>
2018-01-23 22:26:21 +0000

    Add files via upload

---
+++ dbo.Admin.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Admin] (
+    [user_id]  INT          IDENTITY (1, 1) NOT NULL,
+    [username] VARCHAR (20) NOT NULL,
+    [password] VARCHAR (15) NOT NULL,
+    PRIMARY KEY CLUSTERED ([user_id] ASC)
+);
+
---
+++ dbo.places.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[places] (
+    [id]          INT          IDENTITY (1, 1) NOT NULL,
+    [name]        VARCHAR (50) NOT NULL,
+    [county]      VARCHAR (10) NOT NULL,
+    [wet_notwet]  BIT          NOT NULL,
+    [lang_origin] VARCHAR (50) NOT NULL,
+    [etymology]   VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
